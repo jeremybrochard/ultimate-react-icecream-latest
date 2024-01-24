d7015d3c5938a1c9038eb9c500bf5daf98294158
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-01-24 18:44:27 +0100

    feat: add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './styles/ice-cream.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <header>
-      <h1>Your app goes here</h1>
-    </header>
+    <Fragment>
+      <Header></Header>
+      <main></main>
+      <Footer></Footer>
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import iceCreamImage from '../assets/img/ultimate-ice-cream.svg';
+
+const Header = () => (
+  <header>
+    <h1>
+      <img src={iceCreamImage} alt=""></img>
+      Ultimate Ice Cream
+    </h1>
+  </header>
+);
+
+export default Header;
